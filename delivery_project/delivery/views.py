9e82f673bafbb066d9d88c4507c448757d637281
Malika Edaif <edmalika957@gmail.com>
2025-03-30 10:28:18 +0200

    create views login and logout

--- delivery_project/delivery/views.py
+++ delivery_project/delivery/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth import login,logout,authenticate
 
 # Create your views here.
+
+
+def Login_user(request):
+    email = request.POST['email']
+    password =request.POST['password']
+    user=authenticate(request, email=email, password=password)
+    if user is not None:
+        login(request,user)
+        #redirect to the session
+    
+
+def Logout_user(request):
+    logout(request)
+    #redirect to login page
+    
